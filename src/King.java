935d9a1f3c45cd53cdab6983d1f73892127ca5b6
sardor <alimukhammad1@gmail.com>
2023-04-17 21:35:49 -0400

    kings castlingDone

--- src/King.java
+++ src/King.java
@@ -1,5 +1,20 @@
 // King: To represent King as a chess piece
 
 public class King extends Piece {
-    
+    private boolean castlingDone = false;
+
+    public King(boolean white)
+    {
+        super(white);
+    }
+  
+    public boolean isCastlingDone()
+    {
+        return this.castlingDone;
+    }
+  
+    public void setCastlingDone(boolean castlingDone)
+    {
+        this.castlingDone = castlingDone;
+    }
 }
